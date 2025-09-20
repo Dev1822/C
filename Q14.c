b76e49f46721e42e76727454ad2e5c27e9954fda
Dev1822 <dev.d.patel.cg@gmail.com>
2025-09-20 11:58:59 +0530

    Updates

--- Q14.c
+++ Q14.c
@@ -3,13 +3,16 @@
 int main() {
     int n;
     scanf("%d",&n);
+    int count;
     for (int i=1;i<=n;i++){
+        count=0;
         for (int j=1;j<=n+i-1;j++){
             if (j<=n-i){
                 printf(" ");
             }  
             else{
-                printf("*");
+                count++;
+                printf("%d",count);
             }  
         }
         printf("\n");  
